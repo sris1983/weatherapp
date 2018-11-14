975d2292c8ece4cd2c322796485209e1eeddb022
sris1983 <42173381+sris1983@users.noreply.github.com>
2018-11-15 00:26:12 +0530

    Update WeatherApiControllerTest.java

--- weather-app2/src/test/java/com/example/weather/web/WeatherApiControllerTest.java
+++ weather-app2/src/test/java/com/example/weather/web/WeatherApiControllerTest.java
@@ -37,15 +37,13 @@ public class WeatherApiControllerTest {
 		Weather weather = new Weather();
 		weather.setName("London");
 		setWeatherEntry(weather, 286.72, 800, "01d", Instant.ofEpochSecond(1234));
-		//given(this.weatherService.getWeather("uk", "london")).willReturn(weather);
 		this.mvc.perform(get("/api/weather/now/uk/london"))
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$.name", is("London")))
 				.andExpect(jsonPath("$.temperature", is(286.72)))
 				.andExpect(jsonPath("$.weatherId", is(800)))
 				.andExpect(jsonPath("$.weatherIcon", is("01d")))
-				.andExpect(jsonPath("$.timestamp", is("1970-01-01T00:20:34Z")));
-	//	verify(this.weatherService).getWeather("uk", "london");
+				.andExpect(jsonPath("$.timestamp", is("1970-01-01T00:20:34Z")));	
 	}
 
 	@Test
@@ -54,13 +52,7 @@ public class WeatherApiControllerTest {
 		forecast.setName("Brussels");
 		forecast.getEntries().add(createWeatherEntry(285.45, 600, "02d", Instant.ofEpochSecond(1234)));
 		forecast.getEntries().add(createWeatherEntry(294.45, 800, "01d", Instant.ofEpochSecond(5678)));
-		/*given(this.weatherService.getWeatherForecast("be", "brussels")).willReturn(forecast);
-		this.mvc.perform(get("/api/weather/weekly/be/brussels"))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.name", is("Brussels")))
-				.andExpect(jsonPath("$.entries[0].temperature", is(285.45)))
-				.andExpect(jsonPath("$.entries[1].temperature", is(294.45)));
-		verify(this.weatherService).getWeatherForecast("be", "brussels");*/
+	
 		given(this.weatherService.getWeatherForecast( "brussels")).willReturn(forecast);
 		this.mvc.perform(get("/api/weather/weekly/be/brussels"))
 				.andExpect(status().isOk())
@@ -79,9 +71,7 @@ public class WeatherApiControllerTest {
 
 	private static void setWeatherEntry(WeatherEntry entry, double temperature, int id, String icon,
 			Instant timestamp) {
-		entry.setTemperature(temperature);
-		entry.setWeatherId(id);
-		entry.setWeatherIcon(icon);
+		entry.setTemperature(temperature);		
 		entry.setTimestamp(timestamp.getEpochSecond());
 	}
 
